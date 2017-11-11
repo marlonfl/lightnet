8c359521043b5cb0e66483046e6bb47c594290f8
marlonfl <marlonfl@informatik.uni-bremen.de>
2017-11-11 01:18:36 +0100

    update

--- src/nn.py
+++ src/nn.py
@@ -2,36 +2,45 @@ import numpy as np
 
 class FullyConnectedNN(object):
 
-    def __init__(self, layers=(2, 3, 3, 1), batch_size=10, epochs=20,
-                    learning_rate=0.0001):
+    def __init__(self, layers=(2, 3, 1), batch_size=10, epochs=20,
+                    learning_rate=0.001):
         weights = []
         for layer, next_layer in zip(layers, layers[1:]):
             weights.append(np.random.rand(next_layer, layer))
-
+        
         self.weights = weights
         self.batch_size = batch_size
         self.epochs = epochs
         self.learning_rate = learning_rate
 
-    def forward(self, values):
-        outp = values[:]
-        for weight in self.weights:
-            outp = self.relu(np.dot(weight, outp))
-
-        return values
 
     def forward_batch(self, batch):
-        outputs = []
-        for sample in batch:
-            outp = sample[:]
-            for weight in self.weights:
-                outp = self.relu(np.dot(weight, outp))
-            outputs.append(outp)
-
-        return np.array(outputs)
-
-    def backward(self):
-        pass
+        layer_zs = []
+        batch = np.transpose(batch)
+        for weight in self.weights:
+            print("multiplying ", batch, " with", weight)
+            z = np.dot(weight, batch)
+            layer_zs.append(z)
+            batch = self.relu(z)
+            print("result ", batch)
+            
+        return (batch, layer_zs)
+            
+            
+    def compute_gradient(self, loss): 
+        gradient = []
+        for i, hidden_layer in enumerate(weights):
+            if i == len(weights) - 1:
+                pass
+                # output layer weight gradients
+                # how much does total loss change with respect to o1
+                #d_o1 = 
+            #else:
+                # hidden layer weight gradients
+                
+            
+    def update_weights(self, gradient):
+        return self.weights - self.learning_rate * gradient        
 
     def relu(self, sums):
         return np.clip(sums, 0, None)
@@ -45,7 +54,9 @@ class FullyConnectedNN(object):
 
     def compute_loss(self, actual, output):
         # squared euclidean
-        return np.sum((actual-output)**2)
+        print("Output: ", output)
+        print("Actual: ", actual)
+        return np.sum((actual-output)**2, axis=1)
 
     def prepare_inputs(self, inputs):
         return np.array([sample.reshape((len(sample), 1)) for sample in inputs])
@@ -65,7 +76,7 @@ class FullyConnectedNN(object):
             # random order
             ind = np.arange(0, len(inputs))
             np.random.shuffle(ind)
-
+            
             # sampling mini-batches
             for i in range(0, len(ind), batch_size):
                 # building batch using random indices
@@ -74,14 +85,15 @@ class FullyConnectedNN(object):
                 batch_labels = targets[training_indices]
 
                 # forwarding batch through network
-                outputs = self.forward_batch(batch)
+                outputs, layer_zs = self.forward_batch(batch)
 
                 # compute the loss of the current batch and weights
                 loss = compute_loss(batch_labels, outputs)
 
                 # propagate backwards, change weights
-                # todo
-                # todo
+                gradient = nn.compute_gradient(loss, layer_zs)
+                nn.update_weights(gradient)
+
 
         loss = compute_loss(targets, forward_batch(inputs))
         training_losses.append(loss)
@@ -92,10 +104,12 @@ class FullyConnectedNN(object):
 if __name__ == "__main__":
     nn = FullyConnectedNN()
 
-    inp = nn.prepare_inputs(np.array([[-1,1], [2,2]]))
+    #inp = nn.prepare_inputs(np.array([1,1], [2,2]))
+    inp = np.array([[1,1], [2,2]])
     actuals = np.array([[[2]], [[3]]])
+    print(inp)
 
-    pred = nn.forward_batch(inp)
+    pred = nn.forward_batch(inp)[0]
 
     print("Prediction: ", pred)
     print("Loss: ", nn.compute_loss(actuals, pred))
